8dcbd32601552591b477130e6a9a04d445721405
kajasumanie <kkajasu@gmail.com>
2018-05-20 23:39:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,6 @@
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
-
 <div class="container">
     <center><h1 class="well">Bcard Generator</h1></center>
 	<div class="col-lg-12 well">
