4c403cfbcd8633794dc8320e474dcc4d9a8c6f49
kajasumanie <kkajasu@gmail.com>
2018-05-22 23:20:56 +0530

    Update CardGenerator.php

--- CardGenerator.php
+++ CardGenerator.php
@@ -1,10 +1,14 @@
 <?php
-$name = $_GET['name'];
-$name = $_GET['name'];
-$name = $_GET['name'];
-$name = $_GET['name'];
-$name = $_GET['name'];
-$message = "Welcome to Kist, $name";
+$firstname = $_GET['1'];
+$lastname = $_GET['2'];
+$address = $_GET['3'];
+$title = $_GET['4'];
+$company = $_GET['5'];
+$phonenumber = $_GET['6'];
+$emailaddress = $_GET['7'];
+$website = $_GET['8'];
+$photo = $_GET['9'];
+$message = " $firstname"+ "$lastname";
 $length = strlen($message) * 9.3;
 $image = imagecreate($length,20);
 $back = imagecolorallocate($image, 0,0,0);
