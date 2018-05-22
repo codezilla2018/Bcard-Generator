99c417cc3468487f8722525d85e935f13ca7aea7
kajasumanie <kkajasu@gmail.com>
2018-05-22 23:16:03 +0530

    Update index.php

--- index.php
+++ index.php
@@ -14,16 +14,16 @@
 						<div class="row">
 							<div class="col-sm-6 form-group">
 								<label>First Name</label>
-								<input type="text" placeholder="Enter First Name Here.." class="form-control">
+								<input type="text" id='1' placeholder="Enter First Name Here.." class="form-control">
 							</div>
 							<div class="col-sm-6 form-group">
 								<label>Last Name</label>
-								<input type="text" placeholder="Enter Last Name Here.." class="form-control">
+								<input type="text" id='2' placeholder="Enter Last Name Here.." class="form-control">
 							</div>
 						</div>					
 						<div class="form-group">
 							<label>Address</label>
-							<textarea placeholder="Enter Address Here.." rows="3" class="form-control"></textarea>
+							<textarea placeholder="Enter Address Here.." rows="3" id='3' class="form-control"></textarea>
 						</div>	
 						<div class="row">
 								
@@ -31,30 +31,30 @@
 						<div class="row">
 							<div class="col-sm-6 form-group">
 								<label>Title</label>
-								<input type="text" placeholder="Enter Designation Here.." class="form-control">
+								<input type="text" placeholder="Enter Designation Here.." id='4' class="form-control">
 							</div>		
 							<div class="col-sm-6 form-group">
 								<label>Company</label>
-								<input type="text" placeholder="Enter Company Name Here.." class="form-control">
+								<input type="text" placeholder="Enter Company Name Here.." id='5' class="form-control">
 							</div>	
 						</div>						
 					<div class="form-group">
 						<label>Phone Number</label>
-						<input type="text" placeholder="Enter Phone Number Here.." class="form-control">
+						<input type="text" placeholder="Enter Phone Number Here.." id='6' class="form-control">
 					</div>		
 					<div class="form-group">
 						<label>Email Address</label>
-						<input type="text" placeholder="Enter Email Address Here.." class="form-control">
+						<input type="text" placeholder="Enter Email Address Here.." id='7' class="form-control">
 					</div>	
 					<div class="form-group">
 						<label>Website</label>
-						<input type="text" placeholder="Enter Website Name Here.." class="form-control">
+						<input type="text" placeholder="Enter Website Name Here.." id='8' class="form-control">
 					</div>
 					<div class="form-group">
 					<label>Upload Photo : </label>
 <input type="file" class="form-control-file" name="file_img" aria-describedby="fileHelp"> <br><br>
 </div>
-					<button type="button" class="btn btn-lg btn-info">Submit</button>					
+					<button type="button" href='CardGenerator.php' class="btn btn-lg btn-info">Submit</button>					
 					</div>
 				</form> 
 				</div>
