d8f4148cfe215f1b2662a02ed023fedca0345410
kajasumanie <kkajasu@gmail.com>
2018-05-23 23:55:42 +0530

    Update CardGenerator.php

--- CardGenerator.php
+++ CardGenerator.php
@@ -1,19 +1,61 @@
 <?php
-$firstname = $_GET['1'];
-$lastname = $_GET['2'];
-$address = $_GET['3'];
-$title = $_GET['4'];
-$company = $_GET['5'];
-$phonenumber = $_GET['6'];
-$emailaddress = $_GET['7'];
-$website = $_GET['8'];
-$photo = $_GET['9'];
-$message = " $firstname"+ "$lastname";
-$length = strlen($message) * 9.3;
-$image = imagecreate($length,20);
-$back = imagecolorallocate($image, 0,0,0);
-$for = imagecolorallocate($image, 255,255,255);
-imagestring($image,5,5,1,$message,$for);
-header("Content-Type: image/jpeg");
-imagejpeg($image);
+
+require_once("qrlib.php");
+require_once("qrcodegenerator.php");
+
+class BCard
+{
+   $firstname=$_POST["firstname"];
+$lastname=$_POST["lastname"];
+$address=$_POST["address"];
+$title=$_POST["title"];
+$company=$_POST["company"];
+$email=$_POST["email"];
+$website=$_POST["website"];
+$phonenumber=$_POST["phonenumber"];
+    public $template = 1 ;
+    
+   
+    QrCodeForCard();
+        $logopath = $this->qrCodePath;
+        if($this->template == 1){
+            $font = 'Starjout.ttf';
+            $firstname = imagecolorallocate($im, 100, 100, 100);
+            $lastname = imagecolorallocate($im, 220, 100, 200);
+            $company = imagecolorallocate($im, 220, 100, 200);
+            $phonenumber = imagecolorallocate($im, 250, 200, 200);
+           $email = imagecolorallocate($im, 100, 100, 100);
+           $website = imagecolorallocate($im, 220, 100, 200);
+            $title = imagecolorallocate($im, 220, 100, 200);
+          
+            
+      
+        }
+      
+        else{
+            $firstname = imagecolorallocate($im, 100, 100, 100);
+            $lastname = imagecolorallocate($im, 220, 100, 200);
+            $company = imagecolorallocate($im, 220, 100, 200);
+            $phonenumber = imagecolorallocate($im, 250, 200, 200);
+           $email = imagecolorallocate($im, 100, 100, 100);
+           $website = imagecolorallocate($im, 220, 100, 200);
+            $title = imagecolorallocate($im, 220, 100, 200);
+          
+        }
+        
+        
+       $image = imagecreatetruecolor(120, 20);
+       $text_color = imagecolorallocate($image, 233, 14, 91);
+        $text_color = imagecolorallocate($im, 233, 14, 91);
+        imagestring($image,5,5,1,'A Simple Text String',$text_color);
+        header("Content-Type: image/jpeg");
+        imagejpeg($image);
+    }
+   
+   
+   
+
+
+
+
 ?>
